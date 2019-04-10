531573b27068104da8099b68feb1b2723c4aa110
calonge <danielcalongeruiz@gmail.com>
2019-04-10 20:55:23 +0200

    Hola

--- src/P4/SimilitudCoseno.java
+++ src/P4/SimilitudCoseno.java
@@ -4,6 +4,7 @@ public class SimilitudCoseno implements Similitud {
 
     @Override
     public double sim(Long u1, Long u2) {
-        return 0;
+        Map<Long, Double> PreferenciasU1= ModeloDatosClass.getPreferenciasUsuario(u1);
+        Map<Long, Double> PreferenciasU2= ModeloDatosClass.getPreferenciasUsuario(u2);
     }
 }
