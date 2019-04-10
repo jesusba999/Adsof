339710bc75e55bd19ebabc823dcbe6b92eff9f22
calonge <danielcalongeruiz@gmail.com>
2019-04-10 20:42:37 +0200

    Hola

---
+++ src/P4/RecomendadorDeVecinos.java
@@ -0,0 +1,8 @@
+package P4;
+
+public class RecomendadorDeVecinos implements Recomendador{
+    @Override
+    public Recomendacion recomienda(Long u, int longitudRecomendacion) {
+        return null;
+    }
+}
---
+++ src/P4/SimilitudCoseno.java
@@ -0,0 +1,9 @@
+package P4;
+
+public class SimilitudCoseno implements Similitud {
+
+    @Override
+    public double sim(Long u1, Long u2) {
+        return 0;
+    }
+}
