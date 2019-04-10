ef6b2a6c97df38dfdf6c0b86d850cae60f53464f
jesus <jesusba999@gmail.com>
2019-04-10 22:37:15 +0200

    He hecho similitud coseno

--- src/P4/Recomendacion/Similitud/SimilitudCoseno.java
+++ src/P4/Recomendacion/Similitud/SimilitudCoseno.java
@@ -22,15 +22,15 @@ public class SimilitudCoseno implements Similitud {
         Double sumU1U2 = 0.0;
 
         for(Object key : PreferenciasU1.keySet()) {
-            sumU1 += Math.pow(PreferenciasU1.get(key), 2);
+            sumU1 += Math.pow(PreferenciasU1.get(key), 2);  //Suma los ratings al cuadrado de las valoraciones del user1
 
             if(PreferenciasU2.containsKey(key)) {
-                sumU1U2 += PreferenciasU1.get(key) * PreferenciasU2.get(key);
+                sumU1U2 += PreferenciasU1.get(key) * PreferenciasU2.get(key);   //Suma la multiplicacion de los ratings de los dos users (para un mismo item, solo los que han valorado ambos usuarios)
             }
         }
 
         for(Object key : PreferenciasU2.keySet()) {
-            sumU2 += Math.pow(PreferenciasU2.get(key), 2);
+            sumU2 += Math.pow(PreferenciasU2.get(key), 2);  //Suma los ratings al cuadrado de las valoraciones del user2
         }
 
 
