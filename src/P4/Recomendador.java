e0c36fdeee35eff9f087fbd194023b7eaa036c53
calonge <danielcalongeruiz@gmail.com>
2019-04-10 20:37:23 +0200

    Hola

--- src/P4/Recomendador.java
+++ src/P4/Recomendador.java
@@ -1,5 +1,5 @@
 package P4;
 
 public interface Recomendador {
-    public Recomendacion recomienda(Long u, int longitudRecomendacion) throws RecomendacionInvalida
+    public Recomendacion recomienda(Long u, int longitudRecomendacion);
 }
