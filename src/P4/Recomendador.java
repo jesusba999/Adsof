ab03da9c341f7c8e3a7ddef165d47a5377ec54c3
jesus <jesusba999@gmail.com>
2019-04-10 20:47:01 +0200

    Añadido Excepciones

--- src/P4/Recomendador.java
+++ src/P4/Recomendador.java
@@ -1,5 +1,7 @@
 package P4;
 
+import P4.Exceptions.RecomendacionInvalida;
+
 public interface Recomendador {
-    public Recomendacion recomienda(Long u, int longitudRecomendacion);
+    public Recomendacion recomienda(Long u, int longitudRecomendacion) throws RecomendacionInvalida;
 }
