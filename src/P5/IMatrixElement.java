424a52c230e53c398094a7b501b46e47b05c6cf6
calonge <danielcalongeruiz@gmail.com>
2019-05-07 19:13:33 +0200

    Hola

--- src/P5/IMatrixElement.java
+++ src/P5/IMatrixElement.java
@@ -8,10 +8,41 @@ package P5;
 
 
 public interface IMatrixElement<T> {
+
+    /**
+     * Devuelve la coordenada i
+     * @return Coordenada i
+     */
     int getI();
+
+    /**
+     * Devuleve la coordenada j
+     * @return Coordenada j
+     */
     int getJ();
+
+    /**
+     * Devuelve el elemento almacenado
+     * @return Elemento
+     */
     T getElement();
+
+    /**
+     * Modifica el elemento almacenado
+     * @param element Elemento
+     */
     void setElement(T element);
+
+    /**
+     * Convierte la clase a una cadena de caracteres
+     * @return Cadena
+     */
     String toString();
+
+    /**
+     * Funcion que compara dos objetos de tipo IMatrixElement
+     * @param obj Objeto con el que queremos comparar
+     * @return True si son iguales, false si no lo son
+     */
     boolean equals(Object obj);
 }
