7f4e008e449912e80d7bd13e5143181ba2a7b553
calonge <danielcalongeruiz@gmail.com>
2019-05-08 12:10:48 +0200

    Hola

--- src/P5/BasicSimulator.java
+++ src/P5/BasicSimulator.java
@@ -1,5 +1,6 @@
 package P5;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,16 +10,46 @@ import java.util.List;
  */
 
 public class BasicSimulator {
+
+    private Entorno entorno;
+
+    private int tiempo = 0;
+
     /**
      * Constructor del simulador basico
      * @param nrows numero de filas
      * @param ncols numero de columnas
      */
     public BasicSimulator(int nrows, int ncols) {
-
+        this.entorno =  new Entorno(nrows, ncols);
     }
 
-    public void create(IBasicAgent agent, int numero, int i, int j){
+    public void create(IBasicAgent agent, int numero, int i, int j) throws IllegalPositionException{
+        List<IBasicAgent> agentes = new ArrayList<>(numero);
 
+        int k;
+        for(k = 0; k < numero; k++){
+            agentes.add(agent.copy());
+        }
+
+        Cell agentCell = new Cell(i, j , agentes, this);
+
+        if(){
+
+        }
+        else{
+            entorno.addElement(agentCell);
+        }
+    }
+
+    public void run(int pasos) {
+        int i;
+
+        for (i = 0; i < pasos; i++) {
+            System.out.println("++++++++++++++++++++++++++++++++++++++++++");
+            System.out.println("Time = " + tiempo);
+            System.out.println(entorno);
+            tiempo ++;
+        }
     }
 }
--- src/P5/Entorno.java
+++ src/P5/Entorno.java
@@ -2,14 +2,43 @@ package P5;
 
 import java.util.List;
 
+
+
 /**
  * Contiene la clase de un agente basico que implementa la interfaz IBasicAgent
  *
  * @author Jesus Blanco y Daniel Calonge
  */
 public class Entorno extends Matrix<List<IBasicAgent>>{
-
+    /**
+     * Constructor de entorno
+     * @param nRows numero de filas
+     * @param nCols numero de columnas
+     */
     public Entorno(Integer nRows, Integer nCols) {
         super(nRows, nCols);
+    }
+
+
+    public String toString() {
+        int i, j;
+        String cadena = "  ";
+
+        for(i = 0; i < this.getRows(); i++){
+            for(j = 0; j < this.getCols(); j++){
+                try {
+                    if (this.getElementAt(i, j) == null) {
+                        cadena += "· |  ";
+                    }
+                    else {
+                        cadena += String.format("%3s",  this.getElementAt(i,j).getElement().size() + "|  ");
+                    }
+                } catch (IllegalPositionException e) {
+                    e.printStackTrace();
+                }
+            }
+            cadena += "\n  ";
+        }
+
     }
 }
